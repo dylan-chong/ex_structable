0a8e5855f059f3048d9663419c263f91f626553a
Dylan Chong <dylan.chong@icloud.com>
2017-12-26 12:51:58 +1300

    Refactor

--- lib/ex_constructor_validator.ex
+++ lib/ex_constructor_validator.ex
@@ -5,27 +5,17 @@ defmodule ExConstructorValidator do
 
   defmacro __using__(options) do
     quote do
-      import ExConstructorValidator # TODO remove
-
       def new(args) do
-        # TODO key args may be strings
-        arg_keys = args |> Map.new |> Map.keys |> MapSet.new
-        required_keys = @enforce_keys |> MapSet.new
-        if not MapSet.subset?(required_keys, arg_keys) do
-          raise(
-            ArgumentError,
-            "Requires keys #{required_keys |> Enum.to_list |> inspect} "
-            <> "but only #{arg_keys |> Enum.to_list |> inspect} "
-            <> "were present"
-            )
-        end
+        alias ExConstructorValidator.Helper
 
+        Helper.require_keys(args, @enforce_keys)
         create_struct(args)
       end
 
       def create_struct(args) do
         struct(__MODULE__, args)
       end
+
     end
 
     # TODO check args
@@ -33,6 +23,22 @@ defmodule ExConstructorValidator do
     # TODO option to allow fallback to all default args if all args are defaultable
     # TODO option to allow no enforce_keys
     # TODO ? option to not allow nil args
+  end
+
+  defmodule Helper do
+    def require_keys(args, enforce_keys) do
+      arg_keys = args |> Map.new |> Map.keys |> MapSet.new
+      required_keys = enforce_keys |> MapSet.new
+
+      if not MapSet.subset?(required_keys, arg_keys) do
+        raise(
+          ArgumentError,
+          "Requires keys #{required_keys |> Enum.to_list |> inspect} "
+          <> "but only #{arg_keys |> Enum.to_list |> inspect} "
+          <> "were present"
+        )
+      end
+    end
   end
 
 end
