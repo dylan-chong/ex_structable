d77f2fe587abb502e65314a48c60db74867d878b
Dylan Chong <dylan.chong@icloud.com>
2017-12-25 23:29:42 +1300

    Add basic def new generation

--- lib/ex_constructor_validator.ex
+++ lib/ex_constructor_validator.ex
@@ -3,16 +3,36 @@ defmodule ExConstructorValidator do
   Documentation for ExConstructorValidator.
   """
 
-  @doc """
-  Hello world.
+  defmacro __using__(options) do
+    quote do
+      import ExConstructorValidator # TODO remove
 
-  ## Examples
+      def new(args) do
+        # TODO key args may be strings
+        arg_keys = args |> Map.new |> Map.keys |> MapSet.new
+        required_keys = @enforce_keys |> MapSet.new
+        if not MapSet.subset?(required_keys, arg_keys) do
+          raise(
+            ArgumentError,
+            "Requires keys #{required_keys |> Enum.to_list |> inspect} "
+            <> "but only #{arg_keys |> Enum.to_list |> inspect} "
+            <> "were present"
+            )
+        end
 
-      iex> ExConstructorValidator.hello
-      :world
+        create_struct(args)
+      end
 
-  """
-  def hello do
-    :world
+      def create_struct(args) do
+        struct(__MODULE__, args)
+      end
+    end
+
+    # TODO check args
+
+    # TODO option to allow fallback to all default args if all args are defaultable
+    # TODO option to allow no enforce_keys
+    # TODO ? option to not allow nil args
   end
+
 end
--- test/ex_constructor_validator_test.exs
+++ test/ex_constructor_validator_test.exs
@@ -2,7 +2,35 @@ defmodule ExConstructorValidatorTest do
   use ExUnit.Case
   doctest ExConstructorValidator
 
-  test "greets the world" do
-    assert ExConstructorValidator.hello() == :world
+  defmodule ExampleStruct do
+    @enforce_keys [:a]
+    defstruct [:a, b: [2], c: 3]
+    use ExConstructorValidator
   end
+
+  test "new creates with all params" do
+    expected = %ExampleStruct{a: 1, b: 2, c: {4, 5}}
+    assert ExampleStruct.new(a: 1, b: 2, c: {4, 5}) == expected
+  end
+
+  test "new creates with all non-default params" do
+    expected = %ExampleStruct{a: 1, b: 2, c: 3}
+    assert ExampleStruct.new(a: 1, b: 2) == expected
+  end
+
+  test "new fails when required param is not passed (:a)" do
+    assert_raise(
+      ArgumentError,
+      ~r".*:a.*",
+      fn -> ExampleStruct.new([]) end
+    )
+  end
+
+  # TODO test new with missing non-default param
+  # TODO test new with invalid params
+  # TODO test new with wrong name param
+
+  # TODO test new with wrong name param
+
+  # TODO get to work with exconstructor library
 end
