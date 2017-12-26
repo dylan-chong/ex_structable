de1c6b93829d923c363617a8b1b4cedb3a848875
Dylan Chong <dylan.chong@icloud.com>
2017-12-26 22:01:18 +1300

    Add excoveralls

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule ExConstructorValidator.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
     ]
   end
 
@@ -24,6 +31,7 @@ defmodule ExConstructorValidator.Mixfile do
       {:mix_test_watch, "~> 0.5", only: :dev, runtime: false},
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.11", only: :dev},
+      {:excoveralls, "~> 0.8", only: :test},
     ]
   end
 end
