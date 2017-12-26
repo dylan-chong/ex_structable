b629da992c26c556c00c1ac2bdf950844e15da14
Dylan Chong <dylan.chong@icloud.com>
2017-12-26 15:22:12 +1300

    Add require_no_invalid_args

--- lib/ex_constructor_validator.ex
+++ lib/ex_constructor_validator.ex
@@ -4,11 +4,19 @@ defmodule ExConstructorValidator do
   """
 
   defmacro __using__(options) do
+    options = Keyword.merge([
+      require_no_invalid_args: true
+    ], options)
+
     quote do
       def new(args) do
         alias ExConstructorValidator.Helper
 
         Helper.require_keys(args, @enforce_keys)
+        if unquote(Keyword.fetch!(options, :require_no_invalid_args)) do
+          Helper.require_no_invalid_args(args, __MODULE__)
+        end
+
         create_struct(args)
       end
 
@@ -21,13 +29,17 @@ defmodule ExConstructorValidator do
     # TODO check args
 
     # TODO option to allow fallback to all default args if all args are defaultable
-    # TODO option to allow no enforce_keys
+    # TODO NEXT option to allow no enforce_keys
     # TODO ? option to not allow nil args
   end
 
   defmodule Helper do
     def require_keys(args, enforce_keys) do
-      arg_keys = args |> Map.new |> Map.keys |> MapSet.new
+      arg_keys =
+        args
+        |> Map.new
+        |> Map.keys
+        |> MapSet.new
       required_keys = enforce_keys |> MapSet.new
 
       if not MapSet.subset?(required_keys, arg_keys) do
@@ -35,7 +47,30 @@ defmodule ExConstructorValidator do
           ArgumentError,
           "Requires keys #{required_keys |> Enum.to_list |> inspect} "
           <> "but only #{arg_keys |> Enum.to_list |> inspect} "
-          <> "were present"
+          <> "were given"
+        )
+      end
+    end
+
+    def require_no_invalid_args(args, module) do
+      arg_keys =
+        args
+        |> Map.new
+        |> Map.keys
+        |> MapSet.new
+      valid_keys =
+        module
+        |> struct([])
+        |> Map.from_struct
+        |> Map.keys
+        |> MapSet.new
+
+      if not MapSet.subset?(arg_keys, valid_keys) do
+        raise(
+          ArgumentError,
+          "Allowed keys are #{valid_keys |> Enum.to_list |> inspect} "
+          <> "but #{arg_keys |> Enum.to_list |> inspect} "
+          <> "were given"
         )
       end
     end
--- test/ex_constructor_validator_test.exs
+++ test/ex_constructor_validator_test.exs
@@ -2,35 +2,52 @@ defmodule ExConstructorValidatorTest do
   use ExUnit.Case
   doctest ExConstructorValidator
 
-  defmodule ExampleStruct do
+  defmodule ABCStruct do
     @enforce_keys [:a]
     defstruct [:a, b: [2], c: 3]
     use ExConstructorValidator
   end
 
+  defmodule DEStruct do
+    defstruct [:d, :e]
+    use ExConstructorValidator, require_no_invalid_args: false
+  end
+
   test "new creates with all params" do
-    expected = %ExampleStruct{a: 1, b: 2, c: {4, 5}}
-    assert ExampleStruct.new(a: 1, b: 2, c: {4, 5}) == expected
+    expected = %ABCStruct{a: 1, b: 2, c: {4, 5}}
+    assert ABCStruct.new(a: 1, b: 2, c: {4, 5}) == expected
   end
 
   test "new creates with all non-default params" do
-    expected = %ExampleStruct{a: 1, b: 2, c: 3}
-    assert ExampleStruct.new(a: 1, b: 2) == expected
+    expected = %ABCStruct{a: 1, b: 2, c: 3}
+    assert ABCStruct.new(a: 1, b: 2) == expected
   end
 
   test "new fails when required param is not passed (:a)" do
     assert_raise(
       ArgumentError,
       ~r".*:a.*",
-      fn -> ExampleStruct.new([]) end
+      fn -> ABCStruct.new([]) end
     )
   end
 
-  # TODO test new with missing non-default param
-  # TODO test new with invalid params
-  # TODO test new with wrong name param
+  describe "when passing an invalid parameter" do
+    test "new fails with default __using__ options" do
+      assert_raise(
+        ArgumentError,
+        ~r"",
+        fn -> ABCStruct.new([a: 1, invalid: 2]) end
+      )
+    end
 
-  # TODO test new with wrong name param
+    test "new creates when require_no_invalid_args: false" do
+      expected = %DEStruct{d: 1, e: 2}
+      assert DEStruct.new(d: 1, e: 2, invalid: 3) == expected
+    end
+
+  end
+
+  # TODO test new with invalid params
 
   # TODO get to work with exconstructor library
 end
