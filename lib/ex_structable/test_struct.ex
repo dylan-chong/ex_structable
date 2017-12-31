39db24f6653e8f4c60b614631c942b0462d111d8
Dylan Chong <dylan.chong@icloud.com>
2018-01-01 11:24:24 +1300

    Add some code to check by Dialxir

--- lib/ex_structable/test_struct.ex
+++ lib/ex_structable/test_struct.ex
@@ -4,8 +4,18 @@ defmodule TestStruct do # TODO
   # (which are checked when you run `mix dialyzer`).
   # Dialxir does not work on exs files, so will not work on tests.
 
-  @enforce_keys [:length, :x, :y]
+  @enforce_keys [:length]
   defstruct [:length, :x, :y]
 
   use ExStructable
+
+  def create_examples do
+    # These examples should not cause dialyzer errors
+    TestStruct.new([length: 1])
+    TestStruct.new([length: 1], [strict_keys: true])
+
+    TestStruct.new(%{length: 1})
+    |> TestStruct.put(%{x: 1})
+    |> TestStruct.put(%{y: 1}, [strict_keys: false])
+  end
 end
