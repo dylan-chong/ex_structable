ce1ab32450778e2bc650336aa1c1d625144aae34
Dylan Chong <dylan.chong@icloud.com>
2018-01-01 19:38:31 +1300

    Remove accidental test file

--- a.sh
+++
@@ -1,8 +0,0 @@
-TRAVIS_ELIXIR_VERSION="1.5.2"
-TRAVIS_OTP_RELEASE="20.1"
-export PLT_FILENAME=elixir-${TRAVIS_ELIXIR_VERSION}_${TRAVIS_OTP_RELEASE}.plt
-export PLT_LOCATION=$HOME/$PLT_FILENAME
-wget -O $PLT_LOCATION https://raw.github.com/danielberkompas/travis_elixir_plts/master/$PLT_FILENAME
-export PROJECT_NAME=ex_structable
-echo dialyzer --no_check_plt --plt $PLT_LOCATION --no_native _build/test/lib/$PROJECT_NAME/ebin
-dialyzer --no_check_plt --plt $PLT_LOCATION --no_native _build/test/lib/$PROJECT_NAME/ebin
