a73fafb43750a693ba52a4b3affd363380f66266
Dylan Chong <dylan.chong@icloud.com>
2018-01-03 15:31:49 +1300

    Version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExStructable.Mixfile do
   def project do
     [
       app: :ex_structable,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.5",
       package: package(),
       description: description(),
